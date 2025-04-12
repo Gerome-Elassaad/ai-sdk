faa302ad111e86fce4c826d1dfdc2d1c388ea0a8
Gerome Elassaad <repo-booster@proton.me>
2025-04-12 15:35:12 +1000

    initial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import { model, modelID } from "@/ai/providers";
+import { weatherTool } from "@/ai/tools";
+import { streamText, UIMessage } from "ai";
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const {
+    messages,
+    selectedModel,
+  }: { messages: UIMessage[]; selectedModel: modelID } = await req.json();
+
+  const result = streamText({
+    model: model.languageModel(selectedModel),
+    system: "You are a helpful assistant.",
+    messages,
+    tools: {
+      getWeather: weatherTool,
+    },
+  });
+
+  return result.toDataStreamResponse({
+    sendReasoning: true,
+  });
+}
